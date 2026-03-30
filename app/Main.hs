1a6eafe4718bf7cc066af9631baab8851cbd7c46
Isak Hansen <isakhansen@hackerman.local>
2026-03-30 17:30:20 +0200

    brick

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,10 @@
 module Main where
 
-import ParseFeed (parseFeed)
+import Brick
+
+ui :: String -> Widget ()
+ui x = str x
 
 main :: IO ()
 main = do
-  f <- parseFeed
-  print f
+  simpleMain $ ui "Hello, World!"
