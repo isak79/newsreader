3794224122ae3265ae57166078cf3eb752bcc554
Isak Hansen <isakhansen@hackerman.local>
2026-04-06 19:18:07 +0200

    select entry

--- app/Main.hs
+++ app/Main.hs
@@ -8,6 +8,7 @@ import Brick
 import qualified Graphics.Vty as V
 import qualified Data.Text as T
 import Brick.Widgets.Border
+import Brick.Widgets.Border.Style
 
 ui :: String -> Widget ()
 ui = str
@@ -16,11 +17,16 @@ titleAttr, sourceAttr, timeAttr :: AttrName
 titleAttr = attrName "title"
 sourceAttr = attrName "source"
 timeAttr = attrName "time"
+normalBorder = attrName "border"
+selectedBorder = attrName "selBorder"
 
 main :: IO ()
 main = do
   tuiState <- buildState
-  let app = App { appAttrMap      = const $ attrMap V.defAttr [(titleAttr, fg V.blue), (sourceAttr, fg V.yellow)]
+  let app = App { appAttrMap      = const $ attrMap V.defAttr [ (titleAttr, fg V.blue)
+                                                              , (sourceAttr, fg V.yellow)
+                                                              , (normalBorder, fg V.white)
+                                                              , (selectedBorder, bg V.cyan)]
                 , appStartEvent   = return ()
                 , appHandleEvent  = handleTuiEvent
                 , appChooseCursor = neverShowCursor
@@ -39,7 +45,7 @@ data ResourceName = ResourceName
 buildState :: IO TuiState
 buildState = do
   entries <- parseFeed
-  pure TuiState { entries, selectedEntry = 1 }
+  pure TuiState { entries, selectedEntry = 2 }
 
 setSelectedEntry :: Integer -> TuiState -> TuiState
 setSelectedEntry i t = t { selectedEntry = i }
@@ -55,10 +61,15 @@ data TuiState = TuiState { entries :: [Entry]
   deriving Show
 
 drawTui :: TuiState -> [Widget n]
-drawTui ts = [vBox $ map drawEntry $ entries ts]
+drawTui ts = [vBox $ map (drawEntry (selectedEntry ts)) (zip (entries ts) [1,2..] )]
 
-drawEntry :: Entry -> Widget n
-drawEntry e = border $ vBox [drawField (title e) titleAttr, drawField (source e) sourceAttr]
+-- drawEntry :: Entry -> Widget n
+drawEntry selected (e,n) =  border $ withAttr a $ vBox [drawField (title e) titleAttr, drawField (source e) sourceAttr]
+  where 
+    a :: AttrName
+    a = if selected == n then selectedBorder else normalBorder 
+    
+
 
 drawField :: T.Text -> AttrName -> Widget n
 drawField t a = withAttr a $ str $ T.unpack t
