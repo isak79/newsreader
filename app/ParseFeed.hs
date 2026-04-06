c139d150fd9b1f7eee13342618d48cdece052c13
Isak Hansen <isakhansen@hackerman.local>
2026-04-06 23:22:58 +0200

    fixed stupid url bug

--- app/ParseFeed.hs
+++ app/ParseFeed.hs
@@ -3,12 +3,12 @@ module ParseFeed(parseFeed, Entry(..)) where
 import Text.Feed.Import
 import Text.Feed.Query
 import Text.Feed.Types
-import Data.Text (Text)
+import qualified Data.Text as T
 import Data.Time (UTCTime)
 import Data.Maybe (catMaybes)
 
-data Entry = Entry {  title :: Text
-                    , source :: Text
+data Entry = Entry {  title :: T.Text
+                    , source :: T.Text
                     , pubTime :: Maybe UTCTime } 
                     deriving Show
 
@@ -19,10 +19,17 @@ parseFeed = do
 
 toEntry :: Item -> Maybe Entry
 toEntry i = do
-  title   <- getItemTitle i
-  source  <- getItemLink i
+  title0   <- getItemTitle i
+  source0  <- getItemLink i
   pubTime <- getItemPublishDate i
+  let title  = T.strip title0
+      source = cleanUrl source0
   pure Entry {title, source, pubTime}
+
+cleanUrl :: T.Text -> T.Text
+cleanUrl t = case T.words t of
+  (u:_) -> u
+  []    -> T.empty 
 
 entries :: Applicative f => Maybe Feed -> f [Entry]
 entries feed = do
