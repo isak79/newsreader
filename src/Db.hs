eb4d06c8c072b43ff9ebe7be331c1e46de3ac4fa
Isak Hansen <isakhansen@MacBook-Air.local>
2026-04-15 14:08:23 +0200

    fetchentries works!

--- src/Db.hs
+++ src/Db.hs
@@ -77,8 +77,6 @@ dbFeeds :: Table DbFeeds
 dbFeeds = table "feeds" [ #fID :- autoPrimary
                       , #url :- unique ]
 
-
-
 addFeedToMailbox :: (MonadIO m, MonadMask m) => URL -> Text -> m ()
 addFeedToMailbox url mailboxName = withSQLite "newsreader.sqlite" $ do
   mid <- mailboxIDfromName mailboxName
@@ -93,13 +91,12 @@ refreshAll = withSQLite "newsreader.sqlite" $ do
   urlFids <- queryUrlsAndFeedIDs 
   dbEntries0 <- concat <$> traverse (\(url, fid) -> liftIO $ map (toDbEntry fid) <$> parseFeed url) urlFids 
   insert_ dbEntries dbEntries0
-
-
-queryUrlsAndFeedIDs :: MonadSelda m => m [(URL, ID DbFeeds)]
-queryUrlsAndFeedIDs = do
-  fs <- query $ select dbFeeds
-  pure [ (url f, fID f) | f <- fs ]
-
+    where  
+      queryUrlsAndFeedIDs :: MonadSelda m => m [(URL, ID DbFeeds)]
+      queryUrlsAndFeedIDs = do
+        fs <- query $ select dbFeeds
+        pure [ (url f, fID f) | f <- fs ]
+      
 
 mailboxIDfromName :: MonadSelda m => MailboxName -> m [ID DbMailbox]
 mailboxIDfromName mailboxName = query $ do
@@ -113,4 +110,17 @@ initializeTables = withSQLite "newsreader.sqlite" $ do
   tryCreateTable dbEntries
   tryCreateTable dbMailboxes
   tryCreateTable dbFeeds 
+
+
+fetchEntries :: (MonadMask m, MonadIO m) => Text -> m [Entry]
+fetchEntries mailboxName = withSQLite "newsreader.sqlite" $ do
+  result <- query $ do
+    mailbox <- select dbMailboxes 
+    restrict (mailbox ! #name .== literal mailboxName)
+    dbFeed <- select dbFeeds  
+    restrict (dbFeed ! #mailboxID .== mailbox ! #mID)
+    dbEntry <- select dbEntries 
+    restrict (dbEntry ! #feedID .== dbFeed ! #fID)
+    pure dbEntry
+  pure $ map fromDbEntry result
 
